dc35898aef45bbf5cfe6a89cd68ddccf851f9c38
winter <1304879929@qq.com>
2021-08-26 23:41:19 +0800

    add Child.vue modify App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    App.vue 我现在有 {{total}}
+    <hr>
+    <Child/>
+     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Child from './components/Child.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{total:10000}
+  },
+  components: {Child}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border: 3px solid red;
+  padding: 10px;
+  
 }
 </style>
---
+++ src/components/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="child">
+        {{money}}
+        <button><span>花钱</span></button>
+    </div>
+</template>
+<script>
+export default {
+  props:["money"]
+}
+</script>
+<style scoped>
+.child{
+    border: 2px solid green;
+   padding: 5px;
+}
+</style>
