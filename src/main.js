13cc0dbcc96da3b4ff77532daa23a4e948a9ce68
winter <1304879929@qq.com>
2021-08-28 15:39:29 +0800

    v-on事件绑定

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // 非完整版本
+// import Vue from 'vue'
+// import App from './App.vue'
+// 完整版本bootCdn引入
 
-import Vue from 'vue'
-import App from './App.vue'
 
+const Vue = window.Vue
 Vue.config.productionTip = false
+
 // 全局指令v-x，可以在任何组件使用v-x
 Vue.directive('x', {
   inserted: function (el) {
@@ -12,5 +15,15 @@ Vue.directive('x', {
 })
 
 new Vue({
-  render: h => h(App),
+  template: `
+<button v-on:click="hi">点我</button>
+`,
+  methods: {
+    hi() {
+      console.log('hi');
+    }
+
+  }
+  // render: h => h(App),
+
 }).$mount('#app')
