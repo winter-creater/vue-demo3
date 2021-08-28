5b073dce7feeea8878987f4f38d58fce435b4712
winter <1304879929@qq.com>
2021-08-28 13:44:14 +0800

    声明一个局部指令 v-y,只能在当前组件使用

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="child">
         
-        <p v-x>儿子：</p>
+        <p v-x v-y>儿子：</p>
         {{money}}
         <button @click="$emit('update:money',money-100)"><span>花钱</span></button>
     </div>
@@ -10,6 +10,14 @@
 // 不能直接用外部数据，会改变props，父组件会重新渲染  @click="money-=100"不能这么写
 //Vue内置了eventBus,可用当前实例 $emit    @click="$emit('useMoney',money-100)"
 export default {
+    // 声明一个局部指令 v-y,只能在当前组件使用
+    directives:{
+        y:{
+            inserted(el){
+                el.addEventListener('click',()=>{console.log('y');})
+            }
+        }
+    },
   props:["money"]
 }
 </script>
